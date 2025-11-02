f3334b66f6af38d208da13ea19860e56c220edab
eisenaj <eisenaj@eofe7.mit.edu>
2025-11-02 14:35:10 -0500

    switching to uv

--- env_create_uv.sh
+++ env_create_uv.sh
@@ -18,4 +18,5 @@ uv run ipython kernel install --user --env VIRTUAL_ENV "$(pwd)/.venv" --name=Jac
 echo "Environment created successfully!"
 echo "To start Jupyter in Cursor/VSCode, click the kernel selecter in the top right corner."
 echo "Then select the 'Select Another Kernel...' --> 'Jupyter Kernel...' --> 'JacobianODE'"
+echo "In the 'Jupyter Kernel...' selector, you may need to click the refresh button in the top right corner to see the new kernel."
 echo "To activate the environment, from inside this directory, run: source .venv/bin/activate"
